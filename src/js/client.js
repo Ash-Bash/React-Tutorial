748ffb7c742689f0b0fa844590d7adff88b6bf88
Ashley Chapman <Ash-Bash32@hotmail.co.uk>
2018-06-19 13:11:18 +0100

    Tutorial #2 - Basic Reactjs Components & Rendering

--- src/js/client.js
+++ src/js/client.js
@@ -2,9 +2,22 @@ import React from "react";
 import ReactDom from "react-dom";
 
 class Layout extends React.Component {
+    constructor() {
+        super();
+        this.name = "It's Ash-Bash"
+    }
+
+    getVal() {
+        return 2 + 2;
+    }
+
     render() {
+        const helloWorldText = "Hello, World!!!!";
         return (
-            <h1>Hello, World!!!</h1>
+            <div>
+                <h1>{helloWorldText}, 2 + 2 Equals {this.getVal()}</h1>
+                <h2>{this.name}</h2>
+            </div>
         );
     }
 }
